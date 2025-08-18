685e55a12efaa1294b7e113a8ceb0a1f419c05d3
viniersiren <devinstuddard@gmail.com>
2025-08-18 07:15:23 -0700

    why wont my pr work

--- babel.config.js
+++ babel.config.js
@@ -1,22 +1,30 @@
+// module.exports = function (api) {
+//   api.cache(true);
+//   return {
+//     presets: ['babel-preset-expo'],
+//     plugins: [
+//       // Temporarily disabled dotenv plugin
+//       // [
+//       //   'module:react-native-dotenv',
+//       //   {
+//       //     moduleName: '@env',
+//       //     path: '.env',
+//       //     blacklist: null,
+//       //     whitelist: null,
+//       //     safe: false,
+//       //     allowUndefined: true,
+//       //   },
+//       // ],
+//     ],
+//   };
+// };
+// somehow this SIMPLE plugin would cause an error where expo couldnt find any of the
+//other files
+
 module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      // Temporarily disabled dotenv plugin
-      // [
-      //   'module:react-native-dotenv',
-      //   {
-      //     moduleName: '@env',
-      //     path: '.env',
-      //     blacklist: null,
-      //     whitelist: null,
-      //     safe: false,
-      //     allowUndefined: true,
-      //   },
-      // ],
-    ],
+    // Removed the problematic override that was causing metro-react-native-babel-preset errors
   };
 };
-// somehow this SIMPLE plugin would cause an error where expo couldnt find any of the 
-//other files
